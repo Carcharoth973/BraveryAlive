9216bbbc0317e2efe56ebaee72dd3274d5ef9718
Carcharoth973 <leon.rodewald@gmail.com>
2023-03-13 11:36:49 +0100

    refactor

--- src/main/java/Button.java
+++ src/main/java/Button.java
@@ -2,75 +2,56 @@ import processing.core.*;
 
 class Button extends PApplet{
 
-    private final float posX;
-    private final float posY;
-    private final float breite;
-    private final float hoehe;
-    private int[] currentColor;
+    private final float posX, posY, width, height;
+    private int[] currentColor, strokeColor;
     private boolean pressed;
-    private int[] strokeColor;
-    private final int mode;
-    private String name;
+    private final PShape pShape;
+    private final int ellipseMode, shapeMode, form;
 
-    Button(float posX, float posY, float breite, float hoehe, int mode, String name) {
-        this.breite = breite;
-        this.hoehe = hoehe;
+    Button(float posX, float posY, float width, float height, int form, int ellipseMode, int shapeMode, PShape pShape) {
+        this.width = width;
+        this.height = height;
         this.posX = posX;
         this.posY = posY;
-        this.mode = mode;
-        this.name = name;
+        this.form = form;
+        this.shapeMode = shapeMode;
+        this.ellipseMode = ellipseMode;
+        this.pShape = pShape;
         currentColor = new int[] {255,255,255};
         strokeColor = new int[] {180,180,180};
     }
 
-
-    public boolean drawMe(Button b, PShape s, PApplet p) {
+    //draw the Button in the given PApplet
+    public void drawMe(PApplet p) {
         setColor(p);
         p.strokeWeight(1);
         p.stroke(strokeColor[0],strokeColor[1],strokeColor[2]);
         p.fill(currentColor[0],currentColor[1],currentColor[2]);
-        switch (name) {
-            case "copy":
-                p.translate(0, 0, -50);
-                p.shapeMode(CENTER);
-                p.ellipseMode(CORNER);
-                p.ellipse(posX - 13, posY, breite, hoehe);
-                p.translate(0, 0, 50);
-                p.shape(s, posX - 1, posY + 2);
-                break;
-            case "reroll":
-                p.translate(0, 0, -50);
-                p.shapeMode(CORNER);
-                p.ellipseMode(CORNER);
-                p.ellipse(posX - 36, posY, breite, hoehe);
-                p.translate(0, 0, 50);
-                p.shape(s, posX, posY);
-                if (inMargin(p)) {
-                    return true;
-                }
-                break;
-            case "export":
-                p.translate(0, 0, -50);
-                p.shapeMode(CENTER);
-                p.ellipseMode(CORNER);
-                p.ellipse(posX - 26, posY, breite, hoehe);
-                p.translate(0, 0, 50);
-                p.shape(s, posX, posY);
-                break;
-        }
-        return false;
+        p.ellipseMode(ellipseMode);
+        p.ellipse(posX, posY, this.width, this.height);
+        p.shapeMode(shapeMode);
+        p.pushMatrix();
+         if(posX < p.width/2f)
+         p.translate(0,-36,50);
+         else if (posX == p.width/2f)
+         p.translate(0,-25,50);
+         else if(posX > p.width/2f)
+         p.translate(-12,-25,50);
+        p.shape(pShape,posX,posY);
+        p.popMatrix();
     }
 
+    //determine weather the mouse is hovering or holding the button
     public boolean inMargin(PApplet p) {
-        if( mode == ELLIPSE) {
-            return p.mouseX > posX - breite / 2 && p.mouseX < posX + breite / 2 && p.mouseY > posY - hoehe / 2 && p.mouseY < posY + hoehe / 2;
-        } else if (mode == RECT) {
-            return p.mouseX > posX && p.mouseX < posX + breite && p.mouseY > posY && p.mouseY < posY + hoehe;
+        if( form == ELLIPSE && this.width == this.height) {
+            return dist(p.mouseX, p.mouseY, posX, posY) < this.width/2;
+        } else if (form == RECT) {
+            return p.mouseX > posX && p.mouseX < posX + this.width && p.mouseY > posY && p.mouseY < posY + this.height;
         }
         return false;
     }
 
-
+//set the current color depending on mouse position
     private void setColor(PApplet p) {
         if (inMargin(p)) {
             if (pressed) {
@@ -85,11 +66,12 @@ class Button extends PApplet{
         }
     }
 
-
+    public PShape getShape () {
+        return pShape;
+    }
     public void setPressed(boolean b) {
         pressed = b;
     }
-
     public boolean getPressed() {
         return pressed;
     }
